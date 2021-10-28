c88bb9df3ba0ee393ffc30844717c8222bce904c
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-10-28 15:36:58 +0530

    Created Nav Bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Nav() {
+    const [searchTerm, setSearchTerm] = useState('Search...')
+    function handleChange(e) {
+        const target = e.target
+        setSearchTerm(target.value)
+    }
+    function empty(e) {
+        const target = e.target
+        if (target.value === "Search...") {
+            setSearchTerm('')
+        }
+        if (target.value === '') {
+            setSearchTerm('Search...')
+        }
+    }
     return (
-        <nav className="w-full min-h-12 bg-gray-800">
-            <ul className="flex justify-between p-4">
-                <li className="text-white mx-4 my-auto"><a href="/">Home</a></li>
-                <li className="text-white">About me</li>
+        <nav className="w-full min-h-12 bg-gray-700">
+            <ul className="flex justify-between h-12 py-2">
+                <li className="hidden">For Menu Icon</li>
+                <li className="text-gray-200 box-border  ml-8 mr-auto h-4 w-16 text-center hidden sm:block"><a className="text-base block w-32 h-8 mx-auto" href="/">Home</a></li>
+                <li className="text-gray-400 sm:ml-auto sm:mr-8 mx-auto"><input className="w-auto mx-auto h-8 rounded-2xl text-gray-100 bg-gray-900 border-2 border-gray-400 px-3 sm:w-96" type="text" onChange={handleChange} onFocus={empty} onBlur={empty} value={searchTerm}></input></li>
             </ul>
         </nav>
     )
