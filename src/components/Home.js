56dcfd18acbca486b7ad1c06593aa5ef05fb075e
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-11-12 16:49:59 +0530

    Added request failure handling.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,22 @@ import Trending from './Trending'
 
 function Home({ config }) {
     document.title = "Home - Movie Database"
-    return (
-        <>
-            <Trending config={config} find="trending" type='movie' text="in Movies" />
-            <Trending config={config} find="trending" type='tv' text="on TV" />
-            <Trending config={config} find="discover" type='movie' text="Movies" />
-            <Trending config={config} find="discover" type="tv" text="Shows" />
-        </>
-    )
+    if (config.images) {
+        return (
+            <>
+                <Trending config={config} find="trending" type='movie' text="in Movies" />
+                <Trending config={config} find="trending" type='tv' text="on TV" />
+                <Trending config={config} find="discover" type='movie' text="Movies" />
+                <Trending config={config} find="discover" type="tv" text="Shows" />
+            </>
+        )
+    } else {
+        return (
+            <div className='flex justify-center align-middle my-28'>
+                <p className="text-gray-600 font-semibold text-xl mx-auto block">Please check your Internet connection.</p>
+            </div>
+        )
+    }
 }
 
 export default Home
