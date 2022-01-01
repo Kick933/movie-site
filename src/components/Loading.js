21e6f7516904279a52583f078414c3672270b105
Pavitar Kumar <pavitrsheoran@gmail.com>
2022-01-01 16:17:59 +0530

    Created Loading Component

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+    return (
+        <div className="flex w-screen h-screen overflow-hidden justify-center items-center">
+            <p className="text-gray-500 text-3xl animate-bounce border-b-2">We are Loading...</p>
+        </div>
+    )
+}
+
+export default Loading
