5da7c66c734e05034d7c8433a984550b07137cf9
Pavitar Kumar <pavitrsheoran@gmail.com>
2022-01-02 13:59:41 +0530

    Redesigned scroll buttons.

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -56,15 +56,15 @@ function Trending(props) {
         )
     } else {
         return (
-            <div className="w-full sm:p-4 group md:px-8 divide-gray-300 relative">
-                <p className="text-xl border-b-2 text-gray-800">{props.find === 'trending' ? `Trending ${props.text}` : `Explore ${props.text}`}</p>
-                <div className="w-full px-18 flex scroll-smooth">
-                    <button onClick={scrollLeft} className="w-8 md:w-16 h-56 hidden text-white md:h-80 my-4 sm:flex items-center justify-center opacity-0 rounded-l-xl group-hover:opacity-70 z-10 bg-gradient-to-r from-gray-700 to-gray-100"><AiOutlineDoubleLeft color='white' size="2em" /></button>
+            <div className="w-full sm:p-2 group md:px-8 divide-gray-300 text-gray-200 relative">
+                <p className="text-xl font-bold border-b-2 text-gray-200  p-2md:pl-8 pb-2">{props.find === 'trending' ? `Trending ${props.text}` : `Explore ${props.text}`}</p>
+                <div className="w-full flex">
+                    <button onClick={scrollLeft} className="w-8 md:w-16 h-56 hidden text-white md:h-80 my-4 sm:flex items-center justify-center opacity-0 rounded-l-xl group-hover:opacity-70 z-10 bg-gradient-to-r from-gray-400 to-gray-800"><AiOutlineDoubleLeft color='white' size="2em" /></button>
                     <div ref={container} className='flex overflow-x-scroll w-full mx-auto scrollbar-hide py-4'>
                         {trending.results.map(movie => <MovieCard type={props.type} config={config} movie={movie} key={v4()} />)}
-                        {trending.total_pages > trending.page ? <button className='bg-gray-700 bg-opacity-70 text-gray-300 font-bold rounded-xl px-8 md:px-16 mx-2 transform hover:-translate-y-2 w-56 max-h-48 sm:max-h-56 md:max-h-80 transition' onClick={() => handlePage()}>More...</button> : null}
+                        {trending.total_pages > trending.page ? <button className='bg-gray-700 bg-opacity-70 text-gray-300 rounded-xl px-8 md:px-16 mx-2 transform hover:-translate-y-2 w-56 max-h-56 md:max-h-80 transition' onClick={() => handlePage()}>More...</button> : null}
                     </div>
-                    <button onClick={scrollRight} className="w-8 md:w-16 h-56 md:h-80 my-4 hidden sm:flex justify-center items-center opacity-0 rounded-r-xl group-hover:opacity-70 z-10  bg-gradient-to-r to-gray-700 from-gray-100"><AiOutlineDoubleRight color='white' size="2em" /></button>
+                    <button onClick={scrollRight} className="w-8 md:w-16 h-56 md:h-80 my-4 hidden sm:flex justify-center items-center opacity-0 rounded-r-xl group-hover:opacity-70 z-10  bg-gradient-to-r to-gray-400 from-gray-800"><AiOutlineDoubleRight color='white' size="2em" /></button>
                 </div>
             </div >
         )
