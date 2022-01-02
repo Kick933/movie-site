e900863faf67fa6adae2c4344e7268258ef16ca1
Pavitar Kumar <pavitrsheoran@gmail.com>
2022-01-02 13:58:55 +0530

    Chaged Background and Margins.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -10,13 +10,13 @@ function MovieCard(props) {
         return (
             <>
                 <div>
-                    <div className="bg-gray-600 flex-shrink-0 w-32 md:w-48 text-gray-100 rounded-xl m-1 md:mx-4 overflow-hidden transform hover:-translate-y-4 transition shadow-lg">
+                    <div className="bg-gray-600 flex-shrink-0 w-32 md:w-48 text-gray-100 rounded-xl m-1 md:mx-2 overflow-hidden transform hover:-translate-y-4 transition shadow-lg">
                         <Link to={`/media/${movie.id}/${props.type}`}>
                             <img loading="lazy" draggable="false" className="h-56 md:h-80 w-32 md:w-48 object-fill" src={posterUrl} alt="No Poster found" ></img >
                             {/* <canvas style={styles.votingMeter} className="absolute bg-green-400 rounded-full " width="40" height="40"></canvas> */}
                         </Link>
                     </div>
-                    <p className="h-10 text-gray-700 mx-auto font-medium text-center truncate text-base md:text-base p-4 w-32 md:w-48">{movie.name || movie.title}</p>
+                    <p className="h-10 text-gray-300 mx-auto font-medium text-center truncate text-base md:text-base p-1 w-32 md:w-48">{movie.name || movie.title}</p>
                 </div>
             </>
         )
