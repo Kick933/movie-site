3ac2df0f4c839cab114b0f8338c06e4bc6f4dfa8
Pavitar Kumar <pavitrsheoran@gmail.com>
2022-01-02 13:59:08 +0530

    Background chnaged.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,9 +23,9 @@ function Nav() {
         }
     }
     return (
-        <nav className="w-full min-h-12 bg-gray-800">
-            <ul className="flex justify-between h-12 py-2">
-                <li className="text-gray-200 box-border  ml-8 mr-auto h-4 w-16 text-center hidden sm:block"><a className="text-lg block w-32 h-8 mx-auto" href="/">Home</a></li>
+        <nav className="w-full min-h-12 bg-gray-900">
+            <ul className="flex justify-between items-center h-12 ">
+                <li className="text-gray-200 box-border ml-8 mr-auto hover:bg-gray-500 w-max h-full text-center items-center hidden sm:flex"><a className="text-lg block w-32 h-8 mx-auto" href="/">Home</a></li>
                 <li className="text-gray-400 sm:ml-auto sm:mr-8 mx-auto">
                     <form onSubmit={search}>
                         <input
