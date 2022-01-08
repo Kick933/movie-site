042ff3b94b8dd580f8a6bf334d573ba3960eb4aa
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-01-08 12:30:26 +0530

    Added show searching ability.

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -6,63 +6,77 @@ import Loading from './Loading'
 
 function SearchPage(props) {
     const { query } = useParams()
+    const [show, setShows] = useState({
+        total_pages: null,
+        page: null,
+        results: []
+    })
     const [movie, setMovie] = useState({
         total_pages: null,
         page: null,
         results: []
     })
+    const [moviePage,setMoviePage] = useState(1)
+    const [showPage, setShowPage] = useState(1)
     const [loading, setLoading] = useState(true)
     document.title = `${query} - Search Results`
-    const [page, setPage] = useState(1)
 
-    function handlePage() {
+    function handlePage(arg) {
         if (loading) return
-        if (page < movie.total_pages) setPage(page + 1)
-        setLoading(true)
+        if(arg === 'tv' && showPage < show.total_pages) setShowPage(showPage + 1)
+        if(arg === 'movie' && moviePage < movie.total_pages) setMoviePage(moviePage + 1)
     }
 
     useEffect(() => {
-        if (page <= 1) return
-        const fetchData = async () => {
-            const rawData = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`)
+        const fetchData = async (arg,page) => {
+            const rawData = await fetch(`https://api.themoviedb.org/3/search/${arg}?api_key=${process.env.REACT_APP_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`)
             const data = await rawData.json()
-            setMovie(t => {
-                return ({
-                    ...data,
-                    results: [
-                        ...t.results,
-                        ...data.results
-                    ]
+            if(arg === 'movie'){
+            if(moviePage === 1){
+                setMovie(data)
+            } else{
+                setMovie(t => {
+                    return ({
+                        ...data,
+                        results: [
+                            ...t.results,
+                            ...data.results
+                        ]
+                    })
                 })
-            })
-        }
+            }
+        } else if(arg === 'tv'){
+                if(showPage === 1){
+                    setShows(data)
+                }else{
+                    setShows(t => {
+                        return ({
+                            ...data,
+                            results: [
+                                ...t.results,
+                                ...data.results
+                            ]
+                        })
+                    })
+                }
+        } 
+        }   
         try {
-            fetchData()
+            // Reduces requests by checking change in page for each type.
+            if(movie.page !== moviePage) fetchData('movie',moviePage)
+            if(show.page !== showPage) fetchData('tv',showPage)
             setLoading(false)
         } catch (e) {
             console.log(e.message)
         }
-    }, [page, query])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const rawData = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_KEY}&language=en-US&query=${query}&page=1&include_adult=false`)
-            const data = await rawData.json()
-            setMovie(data)
-        }
-        try {
-            if (query !== undefined)
-                fetchData()
-            setLoading(false)
-        } catch (e) {
-            console.log(e.message)
-        }
-    }, [query])
-    if (loading && movie.page === null) {
+    }, [moviePage, showPage, query,movie.page,show.page])
+    if (loading && movie.page === null && show.page === null) {
         return <Loading />
     } else {
         return <>
-            <div className="flex w-full flex-wrap justify-center align-middle gap-2 pt-12">
+        {/* Movie results. */}
+        <h2 className='w-11/12 mx-auto font-bold text-left p-2 text-2xl my-4 text-gray-300 border-b-2'>Movies</h2>
+            <div className="grid w-full grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 justify-items-center items-center overflow-none">
                 {movie.results.length ? movie.results.map(movie =>
                     <MovieCard
                         type="movie"
@@ -71,8 +85,20 @@ function SearchPage(props) {
                         key={v4()} />
                 ) : <p className="mx-auto my-32 self-center justify-self-center">No Results found</p>}
             </div>
-            {movie.total_pages > movie.page ? <button className='bg-gray-700 block my-4 mx-auto font-bold text-gray-200 rounded-xl transform hover:-translate-y-2 w-48 h-48 sm:h-56 md:h-80 transition' onClick={() => handlePage()}>{!loading ? "More..." : "Loading..."}</button> : null}
-
+            {movie.total_pages > movie.page ? <button className='bg-gray-700 block my-4 mx-auto font-bold text-gray-200 rounded-xl transform hover:-translate-y-2 w-48 h-48 sm:h-56 md:h-80 transition' onClick={() => handlePage('movie')}>{!loading ? "More..." : "Loading..."}</button> : null}
+            {/* Tv Show Results here */}
+            <h2 className='w-11/12 mx-auto font-bold text-left p-2 text-2xl my-4 text-gray-300 border-b-2'>Shows</h2>
+            <div className="grid w-full grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 justify-items-center items-center  overflow-none">
+                {show.results.length ? show.results.map(movie =>
+                    <MovieCard
+                        type="tv"
+                        config={props.config}
+                        movie={movie}
+                        key={v4()} />
+                ) : <p className="mx-auto my-32 self-center justify-self-center">No Results found</p>}
+            </div>
+            {show.total_pages > show.page ? <button className='bg-gray-700 block my-4 mx-auto font-bold text-gray-200 rounded-xl transform hover:-translate-y-2 w-48 h-48 sm:h-56 md:h-80 transition' onClick={() => handlePage('tv')}>{!loading ? "More..." : "Loading..."}</button> : null}
+            
         </>
     }
 }
