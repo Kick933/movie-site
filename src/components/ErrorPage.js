f3998804308f1f70d13d9391e8b36bfbf2622f1a
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-01-08 17:02:05 +0530

    Modified text based on props.

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function ErrorPage() {
-    return (
-        <div className='w-screen h-screen bg-gray-800 text-xl text-gray-300'>
-            <p>Something Went Wrong.</p>
-            <Link><button className=''>Go to Home</button></Link>
-            
-        </div>
-    )
+function ErrorPage(props) {
+    const wrongPath = props.wrongPath
+    if(wrongPath === true){
+        return(
+        <div className='w-screen h-screen bg-gray-800 flex flex-col justify-center items-center text-xl text-gray-300'>
+        <p className='h-10 text-gray-300 mx-auto font-medium text-center'>Hey,going offroading....</p>
+        <p className='h-10 text-gray-300 mx-auto font-medium text-center'>Time to be back home.</p>
+        <Link to="/"><button className='w-28 text-lg text-black rounded-lg h-8 bg-green-400 hover:bg-green-500'>Go to Home</button></Link>
+    </div>
+        )
+    }else{
+        return (
+            <div className='w-screen h-screen bg-gray-800 flex flex-col justify-center items-center text-xl text-gray-300'>
+        <p className='h-10 text-gray-300 mx-auto font-medium text-center'>Something went wrong.</p>
+        <p className='h-10 text-gray-300 mx-auto font-medium text-center'>Check your internet connection.</p>
+        <Link to="/"><button className='w-28 text-lg text-black rounded-lg h-8 bg-violet-500 hover:bg-violet-400'>Go to Home</button></Link>
+    </div>
+        )
+    }
+    
 }
 
 export default ErrorPage
