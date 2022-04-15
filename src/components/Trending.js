e936c336c7e95c2f78607bef7f9e2772c702a81a
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-04-15 19:32:12 +0530

    Fixed missing useEffect dependency

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -37,7 +37,7 @@ function Trending(props) {
             console.log(e.message)
         }
 
-    }, [props.type, props.find, page])
+    }, [props.type, props.find, page,loading])
 
     // Handle increenting page
     function handlePage() {
@@ -48,10 +48,10 @@ function Trending(props) {
     }
     // Handle Loading State
     return (
-        <div className=" text-gray-800 dark:text-gray-300 md:px-8 overflow-visible">
+        <div className=" text-gray-800 dark:text-gray-300 md:px-8 overflow-y-visible">
             <p className="text-xl font-bold p-2 pl-4 border-b-2 border-gray-500">{props.find === 'trending' ? `Trending ${props.text}` : `Explore ${props.text}`}</p>
                 {/* <button className="w-8 md:w-16 text-4xl h-56 hidden text-black border-black border-2 md:h-80 my-16 sm:flex items-center justify-center opacity-0 rounded-full hover:opacity-100 z-99 bg-white absolute left-0"><AiOutlineDoubleLeft/></button> */}
-                <div className='flex gap-1 p-2 md:gap-8 md:py-10 overflow-x-scroll'>
+                <div className='flex gap-1 p-2 md:gap-8 md:py-10 overflow-y-visible overflow-x-scroll'>
                     {trending.results.map((movie,index) => <MovieCard type={props.type} movie={movie} data-index={index} key={v4()} />)}
                     {trending.total_pages > trending.page ? <button className='bg-gray-700 text-xl hover:text-white text-gray-300 rounded-xl px-16 mx-2 w-56 md:max-h-80' onClick={() => handlePage()}>{loading ? 'loading...' : 'More...'}</button> : null}
                 </div>
