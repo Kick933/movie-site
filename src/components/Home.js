3d961a0c034b68e0a784da8a44c0da1c399a9bee
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-04-16 10:58:58 +0530

    Fixed unused var.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,13 @@ import React, {useContext} from 'react'
 import { Config } from '../context/Config'
 import Loading from './Loading'
 import Trending from './Trending'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
-    document.title = "Home - Movie Database"
     const {config, loading, error} = useContext(Config)
+    const navigate = useNavigate()
+    document.title = "Home - Movie Database"
+    if(error) navigate('/error')
     return loading ? <Loading /> : (
         <div className='w-screen mt-4 overflow-visible'>
             <Trending config={config} find="trending" type='movie' text="in Movies" />
