26adb1c0991e17fdb11d5bd3df64d379ba20da1a
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-04-17 22:25:53 +0530

    Added Recommend Component and axios data fetching

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import Loading from './Loading'
+import axios from 'axios'
+import Recommend from './Recommend'
 
 
-function MoviePage(props) {
-    const config = props.config
-    const param = useParams()
+function MoviePage({config}) {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    const {id,type} = useParams()
     const [loading, setLoading] = useState(true)
     const [isError, setError] = useState(false)
     const [movieDetail, setMovieDetail] = useState({})
@@ -15,34 +17,34 @@ function MoviePage(props) {
     // Get Movie details.
     useEffect(() => {
         let mounted = true
-        const fetchData = async () => {
-            const rawData = await fetch(`https://api.themoviedb.org/3/${param.type}/${param.id}?api_key=${process.env.REACT_APP_KEY}&language=en-US`)
-            const data = await rawData.json()
+        axios.get(`https://api.themoviedb.org/3/${type}/${id}?api_key=${process.env.REACT_APP_KEY}&language=en-US`)
+        .then(function(res){
             if(mounted){
-                setMovieDetail(data)
+                setMovieDetail(res.data)
                 setLoading(false)
             }
         }
-        try {
-                fetchData()
-        } catch (err) {
-            setError(true)
-            console.log(err.message)
-        }
+        ).catch(function(err){
+            console.log(err)
+            if(mounted){
+                setError(true)
+                setLoading(false)
+            }
+        })
         return () => {
             mounted = false
         }
-    }, [param.id, param.type])
+    }, [id, type])
 
     if (isError){
         return <Navigate to='/error' wrongPath={false} />
     }
     return !loading ?
         (<>
-            <div className="pt-16 w-4/5 mx-auto flex flex-col md:flex-row">
-                {config.images ? <img src={`${config.images.secure_base_url}${config.images.poster_sizes[3]}${movieDetail.poster_path}`} className="max-w-xs block mx-auto md:mx-8 shadow-xl border-4 border-sky-300  rounded-xl" alt="Poster Not Found"></img> : null }
+            <div className="max-w-6xl my-12 shadow-xl rounded-xl p-4 border py-16 w-full mx-auto flex flex-col lg:flex-row">
+                {config.images ? <img src={`${config.images.secure_base_url}${config.images.poster_sizes[3]}${movieDetail.poster_path}`} className="max-w-xs w-full mx-auto lg:mx-8 shadow-xl border-4 border-sky-300 rounded-xl" alt="Poster Not Found"></img> : null }
                 <div className="flex px-4 justify-center">
-                    <div className="mx-auto text-center md:text-left">
+                    <div className="mx-auto text-center lg:text-left">
                         <h1 className="font-bold antialiased text-sky-400 mt-8 font-sans text-4xl">{movieDetail.name || movieDetail.title || movieDetail.original_title || movieDetail.original_name}</h1>
                         <p className="text-lg pb-1 text-gray-800">{movieDetail.tagline}</p>
                         <div className="py-0.5 text-sm text-gray-600">
@@ -59,6 +61,7 @@ function MoviePage(props) {
                 </div>
             </div>
             {/* TODO : Add Cast details. */}
+            <Recommend type={type} id={id} />
         </>) :
         <Loading />
 }
