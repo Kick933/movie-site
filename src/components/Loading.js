b20f4e93a72e2ddaded6df0a16e8975dbdc552a7
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-05-11 11:20:47 +0530

    Background is now transparent

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Loading() {
     return (
-        <div className="flex w-screen h-screen overflow-hidden justify-center items-center">
+        <div className="flex w-screen bg-transparent h-screen overflow-hidden justify-center items-center">
             <p className="text-sky-400 text-3xl animate-bounce">Loading...</p>
         </div>
     )
