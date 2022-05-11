359a44dee7713219d3cc118221d08fea74cc49f4
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-05-11 11:00:24 +0530

    Added first and last child margin

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -12,7 +12,7 @@ function MovieCard(props) {
         const posterSM = `${config.images.secure_base_url}${config.images.poster_sizes[2]}${movie.poster_path}`
         const posterLG = `${config.images.secure_base_url}${config.images.poster_sizes[3]}${movie.poster_path}`
         return (
-                <div className='rounded-xl grow-0 shrink-0 relative contrast-125 group my-10 h-auto md:h-80 bg-white border flex flex-col w-36 md:w-48 text-gray-100 shadow-lg'>
+                <div className='rounded-xl grow-0 first:ml-12 last:mr-12 shrink-0 relative contrast-125 group my-10 h-auto md:h-80 bg-white border flex flex-col w-36 md:w-48 text-gray-100 shadow-lg'>
                     <div className="bg-gray-600 group-hover:z-99 transform md:group-hover:scale-125 md:group-hover:absolute transition-all overflow-hidden flex rounded-xl w-36 md:w-48 text-gray-100 ">
                         <Link to={`/media/${movie.id}/${props.type}`}>
                             <img loading="lazy" draggable="false" onError={() => setText("Poster not available.")} className="h-56 md:hidden brightness-90 dark:brightness-75 w-48 rounded-xl object-fill font-bold" src={posterSM} alt={text} ></img >
