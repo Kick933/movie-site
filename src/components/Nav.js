f83ca4fe7da9eff68e35ecce7b8e2e1741078b7d
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-05-26 10:03:55 +0530

    Improved Menu Design.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,31 +2,22 @@ import React, { useState } from 'react'
 import { Outlet, NavLink } from 'react-router-dom'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { AiOutlineSearch, AiFillCloseCircle} from 'react-icons/ai'
-import { useLocation , useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import Menu from './Menu'
 
 function Nav() {
     const [menuActive, setMenuActive] = useState(false)
-    const location = useLocation()
-    const navigate = useNavigate()
     
     function handleMenu(){
-        if(location.pathname === '/menu'){
-            navigate(-1)
             setMenuActive(prev => !prev)
-        }else{
-            navigate('/menu')
-            setMenuActive(prev => !prev)
-        }
-
     }
     return (
-        <div className='min-h-screen dark:bg-black overflow-x-hidden flex justify-between flex-col'>
+        <div className='min-h-screen transition-all dark:bg-black overflow-x-hidden flex justify-between flex-col'>
         <nav className="h-20 relative w-full dark:bg-gray-900 dark:shadow-none dark:border-gray-400 shadow-xl flex text-2xl text-sky-400 items-center">
             <ul className="flex mx-auto w-11/12 grow-0 justify-between items-center h-12">
-                <li onClick={handleMenu} className="flex text-4xl group text-sky-400 p-2 hover:scale-125 justify-center align-center group m-4">
+                <li onClick={handleMenu} className="flex text-4xl group ease-in-out transition-all text-sky-400 p-2 hover:scale-125 hover:rotate-180 justify-center align-center group m-4">
                     <AiFillCloseCircle className={menuActive ? "" : 'hidden'}/>
-                    <GiHamburgerMenu className={menuActive ? 'hidden transition-all' : ''}/>
+                    <GiHamburgerMenu className={menuActive ? 'hidden' : ''}/>
                     </li>
                 <li>
                     <NavLink className="font-bold bg-transparent block mx-4 h-8 transition transform focus:text-sky-300 focus:outline-none hover:scale-105" to="/">The Movie Site</NavLink>
@@ -38,7 +29,8 @@ function Nav() {
                 </li>
             </ul>
         </nav >
-        <div className='flex flex-col grow-0 shrink-0 justify-center items-center w-full max-h-full'>
+        <div className='flex relative flex-col grow-0 shrink-0 justify-center items-center w-full max-h-full'>
+            {menuActive ? <Menu /> : null}
             <Outlet/>
         </div>
         <Footer />
