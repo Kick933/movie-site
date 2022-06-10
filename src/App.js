ea1e5df0b6fca4f8450790b2030c1d34898657e3
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-06-10 15:27:24 +0530

    Added session provider and useGetSession hook.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Suspense } from 'react'
 import {Route, Routes, Navigate, useLocation } from 'react-router-dom'
 import {Config} from './context/Config'
+import { Session } from './context/Session'
 import Explore from './components/Explore'
 import Home from './components/Home'
 import Nav from './components/Nav'
 import Loading from './components/Loading'
 import Search from './components/Search'
 import { useGetConfig } from './hooks/useGetConfig'
+import { useGetSession } from './hooks/useGetSession'
 const SearchPage = React.lazy(() => import('./components/SearchPage'))
 const ErrorPage = React.lazy(() => import('./components/routes/ErrorPage'))
 const MoviePage = React.lazy(() => import('./components/routes/MoviePage'))
 const LoginPage = React.lazy(() => import('./components/auth-pages/Login'))
 
 function App() {
-  const {config, error, loading} = useGetConfig()
+  const {session} = useGetSession()
+  const { config, error, loading } = useGetConfig()
   const location = useLocation()
-
   if(error && location.pathname !== '/error'){
     console.log(error.message)
-      return <Navigate to='/error' replace={true} wrongPath={false} />
+    return <Navigate to='/error' replace={true} wrongPath={false} />
   }else {
     return (
     <Config.Provider value={{config, loading, error}}>
+      <Session.Provider value={{session: session}}>
             <Routes >
                 <Route path="/" exact element={<Nav />}>
                       <Route index element={<Home />} />
@@ -30,8 +33,7 @@ function App() {
                         <Suspense fallback={<Loading />}>
                         <LoginPage />
                       </Suspense>
-                      }>
-                      </Route>
+                      } />
                       <Route path="/media/:id/:type" element={
                           <Suspense fallback={<Loading/>}>
                             <MoviePage config={config} />
@@ -56,6 +58,7 @@ function App() {
                       } />
                       
             </Routes>
+            </Session.Provider>
     </Config.Provider>
     )
               }
