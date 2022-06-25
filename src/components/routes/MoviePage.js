4d84e6729921f67a866c57b1964e5935aa77d46b
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-06-25 13:36:11 +0530

    Add to watchlist/favorite.

--- src/components/routes/MoviePage.js
+++ src/components/routes/MoviePage.js
@@ -6,14 +6,16 @@ import Recommend from '../Recommend'
 import { useFetch } from '../../hooks/useFetch'
 import Plot from '../abstracts/Plot'
 import Genre from '../abstracts/Genre'
+import { useGetRatings } from '../../hooks/useGetRatings'
+import {AiFillHeart, AiOutlineHeart, AiFillClockCircle, AiOutlineClockCircle} from 'react-icons/ai'
 
 
 function MoviePage() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
     const {config} = useContext(Config)
     let {id,type} = useParams()
+    const {data : ratingData, loading:ratingLoading, error: ratingError, setRating, addWatchlist, addFavorite} = useGetRatings({type,id})
     const url = `https://api.themoviedb.org/3/${type}/${id}?api_key=${process.env.REACT_APP_KEY}&language=en-US`
-    const deps = [url, type, id]
+    let deps = [url, type, id]
     const { data: movieDetail, loading, error} = useFetch(url, deps, {})
     
     if (error){
@@ -33,14 +35,21 @@ function MoviePage() {
                 className="max-w-xs min-w-fit w-full mx-auto lg:mx-8 shadow-xl border-4 border-sky-300 rounded-xl" 
                 alt="Poster Not Found">
                     </img>
-                <div className="flex px-4 justify-center">
-                    <div className="mx-auto text-center lg:text-left">
-                        <h1 className="font-bold antialiased text-sky-400 mt-8 font-sans text-4xl">{movieDetail.name || movieDetail.title || movieDetail.original_title || movieDetail.original_name}</h1>
-                        <p className="text-lg pb-1 text-gray-800">{movieDetail.tagline}</p>
+                <div className="flex px-4 justify-around content-center">
+                    <div className="mx-4">
+                        <div className='flex flex-col mt-8'>
+                            <h1 className="font-bold antialiased text-sky-400 font-sans text-4xl">{movieDetail.name || movieDetail.title || movieDetail.original_title || movieDetail.original_name}</h1>  
+                            <p className="text-lg text-gray-800">{movieDetail.tagline}</p>
+                        </div>
                         <Genre data={movieDetail} />
-                       <Plot data={movieDetail} />
+                        <div className='flex gap-4'>
+                        {!(ratingLoading || ratingError) ?<button onClick={addWatchlist} title={ratingData.watchlist ? "Remove from watchlist" : "Add to watchlist"} className='text-3xl text-black'>{ratingData.watchlist ? <AiFillClockCircle/> : <AiOutlineClockCircle/>}</button> : null}
+                        {!(ratingLoading || ratingError) ? <button onClick={addFavorite} title={ratingData.favorite ? "Remove from favourites" : "Add to favourites"} className='text-3xl text-red-500 flex justify-center items-center'>{ratingData.favorite ? <AiFillHeart/> : <AiOutlineHeart/>}</button> : null}
+                        </div>
+                        <Plot data={movieDetail} />
+                        <button onClick={setRating} className='hidden'>Rate</button> 
+                        {/* Temporarily makefake button to hide no-unsed-vars. Will implement rating functionality soon. */}
                     </div>
-
                 </div>
             </div>
             {/* TODO : Add Cast details. */}
@@ -50,4 +59,4 @@ function MoviePage() {
     return <Loading />
 }
 
-export default React.memo(MoviePage)
+export default MoviePage
