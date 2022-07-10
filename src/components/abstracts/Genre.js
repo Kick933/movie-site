3b22c6ca4f27217a479eed2993941dce1e7f1ebb
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-07-10 12:21:19 +0530

    Refactored styling.

--- src/components/abstracts/Genre.js
+++ src/components/abstracts/Genre.js
@@ -4,9 +4,9 @@
 export default function Genre({data}) {
   return (
     <div className="py-0.5 text-sm text-gray-600">
-        <p className="py-1">Genre : {data.genres.map(i => i.name).join(', ')}</p>
-        <p className="py-1">{data.status}{data.release_date ? `: ${data.release_date}` : null}</p>
-        {data.runtime ? <p className="text-xs text-gray-600 py-1">Runtime: {data.runtime} Minutes</p> : null}
+        <p>Genre : {data.genres.map(i => i.name).join(', ')}</p>
+        <p>{data.status}{data.release_date ? `: ${data.release_date}` : null}</p>
+        {data.runtime ? <p>Runtime: {data.runtime} Minutes</p> : null}
     </div>
   )
 }
