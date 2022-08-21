25e9fc824ca8c489c976827626f753566da76983
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-08-21 12:44:44 +0530

    Added hide functionality for reviews.

--- src/components/abstracts/Reviews.js
+++ src/components/abstracts/Reviews.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi'
 import { useFetch } from '../../hooks/useFetch'
 import ReviewCard from './ReviewCard'
 //
 //
-// TODO : Add Pagination and option to hide revies altogether to declutter interface.
-//
+// TODO : Add Pagination.
 //
 
 // Provides revies for movies as well as shows
@@ -12,10 +12,17 @@ import ReviewCard from './ReviewCard'
 // Takes movie/show id as second argument.
 // Return a card with reviews along with pagination.
 export default function Reviews({type,id}) {
+    const [hidden,setHidden] = useState(true) // hidden state of reviews
+    const ref = useRef(null) // Ref for reviews
     let url = `https://api.themoviedb.org/3/${type}/${id}/reviews?api_key=${process.env.REACT_APP_KEY}&language=en-US&page=1`
     const deps = [type,id]
     const {data : reviews,loading,error} = useFetch(url,deps)
     if(!loading) console.log(reviews)
+    function toggle(){
+        ref.current.classList.toggle('hidden')
+        ref.current.classList.toggle('flex')
+        setHidden(prev => !prev)
+    }
     if(loading) return (
         <div className='max-w-6xl w-full border my-8 shadow-md justify-center p-4 rounded-xl flex'>
             <p>Loading...</p>
@@ -27,14 +34,19 @@ export default function Reviews({type,id}) {
         </div>
     )
     if(reviews.total_results === 0) return (
-        <div className='max-w-6xl w-full border my-8 shadow-md justify-center p-4 rounded-xl flex'>
+        <div className='max-w-7xl w-full border my-8 shadow-md justify-center p-4 rounded-xl flex'>
             <p className='font-bold text-gray-500'>We have no reviews available for this {type === 'tv' ? 'show' : 'movie'}</p>
         </div>
     )
     return (
-        <div className='max-w-6xl w-full flex-col items-center gap-2 border my-8 shadow-md justify-center p-4 rounded-xl flex'>
-            <p className='font-bold text-xl text-gray-700 mb-2 lg:mb-4'>Reviews</p>
-            {reviews.results.map(i => <ReviewCard review={i} key={i.id} />)}
+        <div className='max-w-7xl w-full flex-col items-center gap-2 border my-4 lg:w-11/12 shadow-md justify-center py-4 rounded-xl flex'>
+            <div onClick={toggle} className='flex text-xl text-sky-400 font-bold justify-between md:px-8 border-2 rounded-xl lg:w-11/12 p-2 w-full'>
+                <p className=''>Reviews</p>
+                {hidden ? <BiChevronDown className='text-3xl animate-pulse'/> : <BiChevronUp className='text-3xl animate-pulse' />}
+            </div>
+            <div ref={ref} className='hidden flex-col items-center mx-auto'>
+                {reviews.results.map(i => <ReviewCard review={i} key={i.id} />)}
+            </div>
         </div>
     )
 }
